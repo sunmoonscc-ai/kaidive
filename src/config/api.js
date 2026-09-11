8b94592149b5c1171dc329610a941fb809c03db1
Jun <sunmoon.scc@gmail.com>
2026-09-11 18:42:28 +0900

    20260911_1842

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
 export const GALLERY_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyBuAwi0ts-h-CC8tt7y68AObVnoEeGvQhi4JoRjxH3BMTkZW14SW2ARnxhBGQlgA/exec';
-export const BOOKING_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx-_e1a7H-yqRLab5cjx4EguujyH2U1pRGZ_qiXEfhoZq7ltoGgdMFEx5ej4JSlk_K27g/exec';
+export const BOOKING_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwk8O578x2YmdrNAQ4gy0qgUqz0t0vxZEhqrr5hfWKzbCi2fXZwtlc7u9GoyKne_hpcBg/exec';
